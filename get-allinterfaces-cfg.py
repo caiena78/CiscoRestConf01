a0aafdf9588d0b982dab63a88089ea5ea2b5f058
caiena78 <38800894+caiena78@users.noreply.github.com>
2022-08-02 13:40:33 -0500

    update

--- get-allinterfaces-cfg.py
+++ get-allinterfaces-cfg.py
@@ -38,7 +38,6 @@ def disableCheck(interface,disabledate):
 # set up connection parameters in a dictionary
 device = {"ip": os.getenv('switch_ip'), "port": "443", "user": os.getenv('switch_user'), "password": os.getenv('switch_pwd')}
 
-#device = {"ip": "192.168.164.5", "port": "443", "user": "ca14028", "password": "Ilovemywifeandkids07"}
 
 
 url = f"https://{device['ip']}:{device['port']}/restconf/data/Cisco-IOS-XE-native:native/interface"
